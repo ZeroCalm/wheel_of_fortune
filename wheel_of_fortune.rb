98e5915631d23743fe30c57f4aa302063b86dc7e
zeroCalm <smith.timothyh@gmail.com>
2017-03-01 22:23:01 -0800

    already broke it, THIS one only has two left

--- wheel_of_fortune.rb
+++ wheel_of_fortune.rb
@@ -40,24 +40,20 @@ class WheelOfFortune
   end
 
   def game_over?
-    if $finished_string == @phrase{
-      return true
-    }else{
-      return false
-    }
+    return $finished_string == @phrase
   end
 
 
-#
-# # Checks to see if we executed this file from the command-line
-# # e.g. `ruby simple_jepordy.rb`
-# if __FILE__ == $0
-#
-#   # DRIVER CODE
-#   j = WheelOfFortune.new( {theme: "card games", phrase: "Go fish"} )
-#   j.can_i_have?("g")
-#   j.can_i_have?("o")
-#   p j
-#   puts j # calls j.to_s
-#
-# end
+
+# Checks to see if we executed this file from the command-line
+# e.g. `ruby simple_jepordy.rb`
+if __FILE__ == $0
+
+  # DRIVER CODE
+  j = WheelOfFortune.new( {theme: "card games", phrase: "Go fish"} )
+  j.can_i_have?("g")
+  j.can_i_have?("o")
+  p j
+  puts j # calls j.to_s
+
+end
